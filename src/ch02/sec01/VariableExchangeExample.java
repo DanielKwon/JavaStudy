287cf8e5197f20bda7212fafc1594e0ca73f596a
DanielKwon <joykwon77@gmail.com>
2022-11-07 22:53:59 +0900

    Initial commit

---
+++ src/ch02/sec01/VariableExchangeExample.java
@@ -0,0 +1,24 @@
+package ch02.sec01;
+
+public class VariableExchangeExample {
+    public static void main(String[] args) {
+        /*
+        int x = 3;
+        int y = 5;
+        System.out.println("x:" + x + ", y:" + y);
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x:" + x + ", y:" + y);
+         */
+
+        String a = "Hyukjung";
+        String b = "is";
+        String c = "babo";
+
+        System.out.println(a + " " + b + " " + c);
+        System.out.println(c + " " + b + " " + a);
+    }
+}
---
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,15 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        int value;
+        int result;
+
+        value = 1;
+        result = value + 10;
+
+        System.out.println(result);
+    }
+}
+
+
